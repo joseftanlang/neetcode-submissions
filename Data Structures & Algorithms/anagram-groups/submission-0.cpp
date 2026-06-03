b07d6f1db851ad748b83f58269bf3e074081fe21
Josef Tan <140465588+joseftanlang@users.noreply.github.com>
2026-06-03 22:45:12 +0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagramMap;
+
+        for (const string& word : strs) {
+            string sortedWord = word;
+            sort(sortedWord.begin(), sortedWord.end());
+            anagramMap[sortedWord].push_back(word);
+        }
+
+        vector<vector<string>> result;
+        for (auto& entry : anagramMap) {
+            result.push_back(move(entry.second));
+        }
+        return result;
+    }
+};
